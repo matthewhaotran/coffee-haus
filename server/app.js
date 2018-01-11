8133539b34bb9e112e403623ead2e043612e66fc
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-01-10 20:25:46 -0800

    set up routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const customerRoute = require('./routes/customer.route');
+const productRoute = require('./routes/product.route');
+const saleRoute = require('./routes/sale.route');
+const saleItemRoute = require('./routes/saleitem.route');
 const db = require('./models');
 
 const app = express();
@@ -8,6 +11,11 @@ const app = express();
 app.use(express.static(`${__dirname}/../dist`));
 app.use(bodyParser.json());
 app.use('/api', require('./api'));
+app.use('/api/customer', customerRoute);
+app.use('/api/product', productRoute);
+app.use('/api/sale', saleRoute);
+app.use('/api/saleitem', saleItemRoute);
+
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/../dist/index.html`);
