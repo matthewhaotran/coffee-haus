4e82ae47b4862b1f33aa71cb7d56378eacb40de2
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-01-14 13:47:07 -0800

    finished customer factory

--- client/app/core/customer.factory.js
+++ client/app/core/customer.factory.js
@@ -41,5 +41,21 @@
                         return response.data;
                     })
         }
+
+        function uodate(customer) {
+            return $http
+                    .put('/api/customer/' + customer.id, customer)
+                    .then(function(response) {
+                        return response.data;
+                    })
+        }
+
+        function remove(customer) {
+            return $http
+                    .delete('/api/customer/' + customer.id)
+                    .then(function(response) {
+                        return response.data;
+                    })
+        }
     }
 })();
