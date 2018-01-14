53210433ef943a0da0597c0a63f2e50155936f0d
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-01-14 13:42:13 -0800

    partial customer factory

--- client/app/core/customer.factory.js
+++ client/app/core/customer.factory.js
@@ -9,13 +9,37 @@
 
     function customerFactory($http) {
         var service = {
-            getAll: getAll
+            getAll: getAll,
+            getById: getById,
+            create: create,
+            update: update,
+            remove: remove
         };
 
         return service;
 
         function getAll() {
-            
+            return $http
+                    .get('/api/customer')
+                    .then(function(response) {
+                        return response.data;
+                    });
+        }
+
+        function getById(id) {
+            return $http
+                    .get('/api/customer/' + id)
+                    .then(function(response) {
+                        return response.data;
+                    })
+        }
+
+        function create(customer) {
+            return $http
+                    .post('/api/customer', customer)
+                    .then(function(response) {
+                        return response.data;
+                    })
         }
     }
 })();
