d5c144ad7f7d102a63ca76452f62e03035aa9533
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-01-16 17:54:27 -0800

    reset

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const server = require('./server/app.js');
+const express = require('express');
+const app = express();
+
+app.use(express.static(`${__dirname}`));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', function (req, res) {
+	res.sendFile(__dirname + '/public/about.html');
+});
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
 });
