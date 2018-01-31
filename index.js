9202e4a00ba3520bdd28d1858eb8f4ceb4de14fd
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-01-30 19:39:30 -0800

    connect to database with sequelize

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('heroku_ab732c1c0df9091', 'b9ee42b8f8db04', '9a73dfff', {
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync()
+  .then(() => User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
+
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
