364a070b53683aa393d141adce8ed9b5cc83c1ea
Matthew Tran <matthewhaotran@MacBook-Air.attlocal.net>
2018-02-02 20:22:35 -0800

    configured customer model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,13 @@
-const Sequelize = require('sequelize');
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
 const sequelize = new Sequelize('heroku_ab732c1c0df9091', 'b9ee42b8f8db04', '9a73dfff', {
   host: 'us-cdbr-iron-east-05.cleardb.net',
   dialect: 'mysql',
@@ -12,15 +21,35 @@ const sequelize = new Sequelize('heroku_ab732c1c0df9091', 'b9ee42b8f8db04', '9a7
   operatorsAliases: false
 });
 
-const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE
-});
-
-sequelize.sync()
-  .then(() => User.findAll())
-  .then(jane => {
-    console.log(jane);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
 
-  module.exports = sequelize;
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(function(file) {
+    console.log(file);
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+sequelize.sync();
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
