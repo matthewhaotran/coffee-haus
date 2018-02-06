7ff1fef73d4119ea3654e50c0c17e474fbe44a7a
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-02-05 20:22:54 -0800

    trying app.js

--- server/app.js
+++ server/app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const customerRoute = require('./routes/customer.route');
-const productRoute = require('./routes/product.route');
-const saleRoute = require('./routes/sale.route');
-// const saleItemRoute = require('./routes/saleItem.route');
-const db = require('./models');
+
 const app = express();
+
+
 app.use(bodyParser.json());
-// app.use(express.static(`${__dirname}`));
 app.use(express.static(`${__dirname}/../dist`));
 
-app.use('/api/customer', customerRoute);
-app.use('/api/product', productRoute);
-app.use('/api/sale', saleRoute);
-// app.use('/api/saleItem', saleItemRoute);
 
 app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/../dist/index.html`);
-  });
+  res.sendFile(`${__dirname}/../dist/index.html`);
+});
 
 module.exports = app;
