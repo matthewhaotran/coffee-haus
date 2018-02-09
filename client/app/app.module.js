08802196665b094439c38226138edaa5e0c4480f
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-02-08 21:41:57 -0800

    starting customer factory

---
+++ client/app/app.module.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules
+        'app.core'
+        // Custom modules
+
+        // 3rd Party Modules
+
+    ]);
+})();
