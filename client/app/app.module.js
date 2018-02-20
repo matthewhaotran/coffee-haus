b2ce303fc46187d9a04b41177c30debee849abf0
Matthew Tran <matthewhaotran@Matthews-Air.attlocal.net>
2018-02-19 16:05:59 -0800

    added dashboard ui view

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -3,11 +3,25 @@
 
     angular.module('app', [
         // Angular modules
-        'app.core',
+        
+
         // Custom modules
+        'app.core',
 
         // 3rd Party Modules
         'ui.router'
 
-    ]);
+    ]).config(appConfig);
+
+    appConfig.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+    function appConfig($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/dashboard');
+
+        $stateProvider.state('dashboard', {
+            url: '/dashboard',
+            controller: 'dashboardController as dashboardCtrl',
+            templateUrl: 'app/dashboard/dashboard.template.html'
+        });
+    };
 })();
